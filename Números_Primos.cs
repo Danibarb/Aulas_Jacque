ac7f0e4d7c2391e7a7eb1ddb9c846677a53a3987
Danielle Elaine Barbosa <dani.barb3@hotmail.com>
2022-02-03 20:50:17 -0300

    Adicionar arquivos de projeto.

---
+++ Números_Primos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lacos_de_Repeticao
+{
+    //Classe para a construção dos laço que imprimirá os Números Primos de 0 a 1000.
+    internal class Números_Primos
+    {
+        public int i;
+
+        public void Laco_for()
+        {
+            Console.WriteLine("Números Primos de 1 a 100....");
+            for (i = 1; i <= 100; i++)
+            {
+                int cont = 0;
+
+                for (int n = i; n > 0; n--)
+                {
+                    if (i % n == 0)
+                    {
+                        cont++;
+                    }
+                    if (cont > 2)
+                    {
+                        break;
+                    }
+                    /*condição sempre será verdadeira quando chegar em 2 divisões, não finalizando o 2º for 
+                    if (cont == 2)
+                    {
+                        Console.WriteLine(i);
+                    }*/
+                    
+                }
+            if (cont == 2)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+        Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lacos_de_Repeticao
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Números_Primos primos = new Números_Primos();
+            primos.Laco_for();
+            
+        }
+    }
+}
+
+
